4cad2a5f6005355434a5b630b7076d8cedafbb05
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-06 14:57:26 +0000

    Prints out number of stars in block

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks user for number between 1 and 10 and prints out that number of stars in a box
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to input a number between 1 and 10
+    System.out.println("Please enter a number between 1 and 10");
+    // create a variable for user input
+    int integer = input.nextInt();
+    
+    // prints out first star in row
+    for(int count = 1; count <= integer; count++){
+      System.out.print("*");
+      // prints out the following
+      for( int i = 1; i < integer; i++){
+        System.out.print("*");
+        //  then puts stars on different lines
+      } System.out.println(" ");
+      }
+    
+    
     
   }
 }
